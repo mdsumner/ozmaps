5f788d82589348db6552b8ecbfe1e41720fe958e
Michael Sumner <mdsumner@gmail.com>
2019-11-04 04:22:07 +0000

    use xz compression

--- data-raw/abs.R
+++ data-raw/abs.R
@@ -1,7 +1,11 @@
 ## ozmaps.data version 0.0.1
 ##these are simplified versions of a core set, see ozmaps.data for details
-file.copy(list.files("../ozmaps.data/data", pattern = "^abs.*\\.rda$", full.names = TRUE),
-          "data")
+f <- list.files("../ozmaps.data/data", pattern = "^abs.*\\.rda$", full.names = TRUE)
+fs <- c(grep("ced",f, value = TRUE),
+        grep("lga",f, value = TRUE),
+        grep("ste",f,value = TRUE))
+file.copy(fs,
+          "data/")
 
 library(ozmaps)
 library(tibble)
